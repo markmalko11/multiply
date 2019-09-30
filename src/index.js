62c2489de57fe3129b8d5fcd1a635b437beb5271
markmalko11 <padshik11101989@gmail.com>
2019-09-30 23:10:57 +0300

    finish task

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 module.exports = function multiply(first, second) {
-	let firstNumber = BigInt(first);
+  let firstNumber = BigInt(first);
   let secondNumber = BigInt(second);
   let result = String(firstNumber*secondNumber);
   return result;
